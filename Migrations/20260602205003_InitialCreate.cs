91b201f81115016713e0d2381c98eca4a25a5661
sofia22398-collab <sofia22398@hotmail.com>
2026-06-02 14:51:05 -0600

    Agregar migracion inicial

---
+++ Migrations/20260602205003_InitialCreate.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace CanchaGO.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Canchas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    TipoDeporte = table.Column<string>(type: "text", nullable: false),
+                    TipoSuperficie = table.Column<string>(type: "text", nullable: false),
+                    Interior = table.Column<bool>(type: "boolean", nullable: false),
+                    Iluminacion = table.Column<bool>(type: "boolean", nullable: false),
+                    CantidadJugadores = table.Column<int>(type: "integer", nullable: false),
+                    PrecioHora = table.Column<decimal>(type: "numeric", nullable: false),
+                    Activa = table.Column<bool>(type: "boolean", nullable: false),
+                    Descripcion = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Canchas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuarios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nombre = table.Column<string>(type: "text", nullable: false),
+                    Correo = table.Column<string>(type: "text", nullable: false),
+                    PasswordHash = table.Column<string>(type: "text", nullable: false),
+                    Telefono = table.Column<string>(type: "text", nullable: true),
+                    Rol = table.Column<string>(type: "text", nullable: false),
+                    Activo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuarios", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HorariosBloqueados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CanchaId = table.Column<int>(type: "integer", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HoraInicio = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    HoraFin = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    Motivo = table.Column<string>(type: "text", nullable: true),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HorariosBloqueados", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HorariosBloqueados_Canchas_CanchaId",
+                        column: x => x.CanchaId,
+                        principalTable: "Canchas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reservas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    CanchaId = table.Column<int>(type: "integer", nullable: false),
+                    Fecha = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HoraInicio = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    HoraFin = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    CantidadJugadores = table.Column<int>(type: "integer", nullable: false),
+                    TipoPartido = table.Column<string>(type: "text", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false),
+                    Observaciones = table.Column<string>(type: "text", nullable: true),
+                    EstadoPago = table.Column<string>(type: "text", nullable: false),
+                    MontoTotal = table.Column<decimal>(type: "numeric", nullable: false),
+                    MetodoPago = table.Column<string>(type: "text", nullable: true),
+                    FechaPago = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reservas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Canchas_CanchaId",
+                        column: x => x.CanchaId,
+                        principalTable: "Canchas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Reservas_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Partidos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ReservaId = table.Column<int>(type: "integer", nullable: false),
+                    UsuarioCreadorId = table.Column<int>(type: "integer", nullable: false),
+                    Titulo = table.Column<string>(type: "text", nullable: false),
+                    Nivel = table.Column<string>(type: "text", nullable: true),
+                    CuposTotales = table.Column<int>(type: "integer", nullable: false),
+                    Estado = table.Column<string>(type: "text", nullable: false),
+                    FechaCreacion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Partidos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Reservas_ReservaId",
+                        column: x => x.ReservaId,
+                        principalTable: "Reservas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Partidos_Usuarios_UsuarioCreadorId",
+                        column: x => x.UsuarioCreadorId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PartidoJugadores",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PartidoId = table.Column<int>(type: "integer", nullable: false),
+                    UsuarioId = table.Column<int>(type: "integer", nullable: false),
+                    FechaUnion = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PartidoJugadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PartidoJugadores_Partidos_PartidoId",
+                        column: x => x.PartidoId,
+                        principalTable: "Partidos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PartidoJugadores_Usuarios_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuarios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HorariosBloqueados_CanchaId",
+                table: "HorariosBloqueados",
+                column: "CanchaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartidoJugadores_PartidoId",
+                table: "PartidoJugadores",
+                column: "PartidoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PartidoJugadores_UsuarioId",
+                table: "PartidoJugadores",
+                column: "UsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_ReservaId",
+                table: "Partidos",
+                column: "ReservaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Partidos_UsuarioCreadorId",
+                table: "Partidos",
+                column: "UsuarioCreadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_CanchaId",
+                table: "Reservas",
+                column: "CanchaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reservas_UsuarioId",
+                table: "Reservas",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HorariosBloqueados");
+
+            migrationBuilder.DropTable(
+                name: "PartidoJugadores");
+
+            migrationBuilder.DropTable(
+                name: "Partidos");
+
+            migrationBuilder.DropTable(
+                name: "Reservas");
+
+            migrationBuilder.DropTable(
+                name: "Canchas");
+
+            migrationBuilder.DropTable(
+                name: "Usuarios");
+        }
+    }
+}
