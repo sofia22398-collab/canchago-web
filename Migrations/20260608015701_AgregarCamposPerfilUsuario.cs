c0e5c65a4ac48f7aa06cba2c20b8c7e283d853fa
sofia22398-collab <sofia22398@hotmail.com>
2026-06-07 19:57:14 -0600

    Agregar migracion campos perfil usuario

---
+++ Migrations/20260608015701_AgregarCamposPerfilUsuario.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CanchaGO.Migrations
+{
+    /// <inheritdoc />
+    public partial class AgregarCamposPerfilUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FechaNacimiento",
+                table: "Usuarios",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genero",
+                table: "Usuarios",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FechaNacimiento",
+                table: "Usuarios");
+
+            migrationBuilder.DropColumn(
+                name: "Genero",
+                table: "Usuarios");
+        }
+    }
+}
